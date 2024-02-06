bd6f5d201d3093c4c53991dc8aa05b1b29ad5f2d
Rodrigo Moreno <750694@student.dusd.net>
2024-02-06 14:24:01 -0800

    Added a look at script
    I added a script that looks at an object that falls or is moving

--- ScriptingProjectMoreno/Assets/Scripts/TransformFunctions.cs
+++ ScriptingProjectMoreno/Assets/Scripts/TransformFunctions.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Transform : MonoBehaviour
+public class TransformFunctions : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float turnSpeed = 50f;
